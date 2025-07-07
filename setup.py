187689097c0fd1c5f6872bf2d14830553cd0e1e1
sai <saikiranmandapati@gmail.com>
2025-07-07 10:35:34 +0530

    template_created_using_cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this is an end to end mlops pipeline',
+    author='sai kiran',
+    license='MIT',
+)
